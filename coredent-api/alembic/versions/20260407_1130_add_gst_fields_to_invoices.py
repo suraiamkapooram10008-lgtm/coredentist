7525b1a70590c0a54a8ebf82b0b38d7048193f69
suri103 <97815599+suri103@users.noreply.github.com>
2026-04-09 05:35:37 +0530

    fix: Correct migration chain - GST migration now revises 001_initial
    - Fixed 'Multiple head revisions' error in Alembic
    - Changed down_revision from None to '001_initial' in GST migration
    - Migration chain is now linear: 001_initial -> GST -> indexes -> subscriptions -> lockout -> email_verification
    - Migrations will now run successfully with 'alembic upgrade head'

--- coredent-api/alembic/versions/20260407_1130_add_gst_fields_to_invoices.py
+++ coredent-api/alembic/versions/20260407_1130_add_gst_fields_to_invoices.py
@@ -1,7 +1,7 @@
 """Add GST fields to invoices table
 
 Revision ID: 20260407_1130
-Revises: previous_revision
+Revises: 001_initial
 Create Date: 2026-04-07 11:30:00.000000
 """
 from typing import Sequence, Union
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '20260407_1130'
-down_revision: Union[str, None] = None  # Standalone migration - applies to fresh databases
+down_revision: Union[str, None] = '001_initial'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
